dff68bf7a62f295d508d653ca1a9c5f3e532d55b
Matheus Almeida Da Silva <matalmei@c2r4p2.42sp.org.br>
2026-07-25 17:30:33 -0300

    now is correct? maybe

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: matheusms1 <matheusms1@student.42.fr>      +#+  +:+       +#+        */
+/*   By: matalmei <matalmei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 18:52:44 by matheusms1        #+#    #+#             */
-/*   Updated: 2026/06/22 10:50:44 by matheusms1       ###   ########.fr       */
+/*   Updated: 2026/07/25 16:31:29 by matalmei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,22 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-    const unsigned char *p1;
-    const unsigned char *p2;
-    size_t i;
+	const unsigned char	*p1;
+	const unsigned char	*p2;
+	size_t				i;
 
-    p1 = s1;
-    p2 = s2;
-    i = 0;
-    if (n == 0)
-      return (0);
-    while(i < n)
-    {
-        if (p1[i] != p2[i])
-            return (p1[i] - p2[i]);
-        i++;
-    }
-    return (0);
+	p1 = s1;
+	p2 = s2;
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (i < n)
+	{
+		if (p1[i] != p2[i])
+			return (p1[i] - p2[i]);
+		i++;
+	}
+	return (0);
 }
 
 // int	main(void)
